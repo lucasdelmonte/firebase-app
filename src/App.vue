321cec2b3b4973c6a6796f681428b1d363019ff1
Lucas Delmonte <82593976+lucasdelmonte@users.noreply.github.com>
2023-05-29 20:46:18 -0300

    install pinia

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link>
       <router-link to="/login">Login</router-link>
       <router-link to="/register">Register</router-link>
+      <button>Logout</button>
     </nav>
     <router-view></router-view>
   </div>
