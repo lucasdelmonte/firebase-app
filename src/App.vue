44c883c4ea19089a4e870036e94f47cc68352d5a
Lucas Delmonte <82593976+lucasdelmonte@users.noreply.github.com>
2023-05-29 20:44:17 -0300

    router config

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    <h1>App</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/login">Login</router-link>
+      <router-link to="/register">Register</router-link>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <script setup>
-
+  
 </script>
